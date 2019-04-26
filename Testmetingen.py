ec3c98ded71b1d181dabc8b42d29d262ce5c4acd
EwoutvanderVelde <46598471+EwoutvanderVelde@users.noreply.github.com>
2019-04-26 12:04:55 +0200

    Update Testmetingen.py

--- Testmetingen.py
+++ Testmetingen.py
@@ -13,18 +13,20 @@ schatter = np.mean(dia)
 std = np.std(dia, ddof=1)/np.sqrt(len(dia))         # Onzekerheid
 print("De diameter = {:.2f} +- {:.2f} mm".format(schatter, std)) 
 
-# Als geheel getal dan is er constuctieve interferentie
-n = 1 #brekingsindex stof tussen de lens en glasplaatje
-labda = 546.08 # We werkem met het groene filter
-
-fi = (2*n*schatter)/labda + 1/2
-print ("fi =",fi) # wanneer fi = 0.5 hebben we uitdoving
-
 temp1 = np.mean([5.21, 5.21, 5.21, 5.21]) # Voorbeeld berekening kromtestraal 
 temp2 = np.mean([5.00, 5.00, 5.00, 5.01])
 
 a = 30.1
-d = temp1 - temp2
+dikte = temp1 - temp2
 
-R = (a**2)/(6*d) + d/2
+R = (a**2)/(6*dikte) + dikte/2
 print("Kromtestraal = {:.3f} mm".format(R)) 
+
+d = R - np.sqrt(R**2 - (schatter/2)**2)
+
+# Als fi een geheel getal dan is er constuctieve interferentie
+n = 1 #brekingsindex stof tussen de lens en glasplaatje
+labda = 546.08 # We werkem met het groene filter
+
+fi = (2*n*d)/labda + 1/2
+print ("fi =",fi) # wanneer fi = 0.5 hebben we uitdoving
